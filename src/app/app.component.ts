df79d6da1f68ea1c6afb2c70b78343a8e554ec2a
eli <ebezhiasvhili@gmail.com>
2024-03-12 07:40:53 -0700

    first homework N2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'commschooltwo';
+  agreeCount = 0;
+  disagreeCount = 0;
+
+  onChoiseMade(choice: boolean) {
+    if (choice) {
+      this.agreeCount++;
+    } else {
+      this.disagreeCount++;
+    }
+  }
 }
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss'],
+})
+export class UserComponent {
+  @Output() choiceMade = new EventEmitter<boolean>();
+  eventEmitted = false;
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onChoiseMade(choice: boolean) {
+    if (!this.eventEmitted) {
+      this.choiceMade.emit(choice);
+      this.eventEmitted = true;
+    }
+  }
+}
